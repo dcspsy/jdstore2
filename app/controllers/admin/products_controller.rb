87fd3154f4ed8b51f48b6b44a3edf8e9111d1f4b
pan <dcspsy@gmail.com>
2017-02-16 10:37:32 +0800

    add admin:product crud

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @products = Product.all
 
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -16,8 +21,22 @@ class Admin::ProductsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
 
   end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
 
   private
   def product_params
